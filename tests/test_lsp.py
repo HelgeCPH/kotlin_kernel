0479b0fcfbcd40340cced4fea440222287ccbe14
fwcd <fwcdmail@gmail.com>
2019-02-24 18:31:17 +0100

    Improved the LSP test by using platform-independent URIs, adding a method to dynamically swap the binary path and abstracting the message parser

--- tests/test_lsp.py
+++ tests/test_lsp.py
@@ -2,15 +2,26 @@ import os
 import json
 import pytest
 import platform
+import pathlib
 from .context import kotlin_kernel as kk
 from subprocess import Popen, PIPE, STDOUT
 
 
-EXAMPLE_FILE = './Hello.kt'
-EXAMPLE_FILE = os.path.abspath(EXAMPLE_FILE)
+EXAMPLE_FILE = os.path.join(__file__, '..', 'Hello.kt')
+EXAMPLE_FILE = pathlib.Path(os.path.abspath(EXAMPLE_FILE)).as_uri()
 
 BINARY = '../kotlin_kernel/java/bin/kotlin-language-server'
 BINARY = os.path.abspath(BINARY)
+
+
+def set_binary(path):
+    """
+    Sets the path to the language server binary. This function
+    can be used to conveniently swap the binary at runtime or
+    in an interactive shell.
+    """
+    global BINARY
+    BINARY = os.path.abspath(path)
 
 
 def send_header(proc, payload_length):
@@ -39,6 +50,28 @@ def send_body(proc, payload):
     """
     print(payload.encode('utf-8'))
     proc.stdin.write(payload.encode('utf-8'))
+    proc.stdin.flush()
+
+
+def read_message(proc):
+    """
+    Reads a message from the language server.
+    """
+    content_length = int(proc.stdout.readline()[16:])
+    proc.stdout.readline()  # read the \r\n
+    response = json.loads(proc.stdout.read(content_length))
+    if 'error' in response:
+      error = response['error']
+      print(error['message'])
+      print(error['data'].replace(r'\r\n', os.linesep))
+    return response
+
+
+def is_method(msg, method):
+    """
+    Tests whether a JSON-RPC message references a given method.
+    """
+    return ('method' in msg) and (msg['method'] == method)
 
 
 def test_hover():
@@ -48,7 +81,7 @@ def test_hover():
             'method': 'textDocument/hover',
             'params': {
               'textDocument': {
-                'uri': f'file://{EXAMPLE_FILE}'
+                'uri': EXAMPLE_FILE
               },
               'position': {
                 'line': 1,
@@ -63,16 +96,10 @@ def test_hover():
     p = Popen([BINARY], stdout=PIPE, stdin=PIPE, stderr=PIPE)
 
     # get the first message after start
-    content_length = int(p.stdout.readline()[16:])
-    p.stdout.readline()  # read the \r\n
-    response = json.loads(p.stdout.read(content_length))
-    print(response)
+    print(read_message(p))
 
     # get the second message after start
-    content_length = int(p.stdout.readline()[16:])
-    p.stdout.readline()  # read the \r\n
-    response = json.loads(p.stdout.read(content_length))
-    print(response)
+    print(read_message(p))
 
     # Now, ask for the hover message
     send_header(p, len(payload_json_utf8))
@@ -80,9 +107,9 @@ def test_hover():
     #written_bytes = p.stdin.write(msg)
     #assert written_bytes == len(msg)
 
-    content_length = int(p.stdout.readline()[16:])
-    p.stdout.readline()  # read the \r\n
-    response = json.loads(p.stdout.read(content_length))
-    print(response)
-
+    response = {'method': ''}
+    while not is_method(response, 'textDocument/hover'):
+      response = read_message(p)
+      print(response)
+    
     assert response.decode('utf-8').contains('inline fun')
